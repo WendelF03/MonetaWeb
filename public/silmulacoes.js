8124b53a25b0a93fc2188da2068b9bfb0cd9a70c
Wendel Pedro <wendelpedro.f@gmail.com>
2022-06-01 03:12:55 -0300

    Atualização

--- public/silmulacoes.js
+++
@@ -1,9 +0,0 @@
-  var simular = document.getElementById("simular");
-  var valor = document.getElementById("valor");
-  var taxa = document.getElementById("taxa");
-  var nPrestacao = document.getElementById("nPrestacao");
-  
-simular.onclick = function simular(){
-  var test = (1000*0.06) + (1000/12);
-  document.getElementById("resultado").innerHTML = test;
-}
